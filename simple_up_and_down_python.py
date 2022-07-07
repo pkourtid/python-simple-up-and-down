4da9d54e31e633ada9af6d7ae443bc44c639371e
Panagiotis Kourtidis <32780481+pkourtid@users.noreply.github.com>
2022-07-07 14:22:54 -0500

    Added logic for animation

--- simple_up_and_down_python.py
+++ simple_up_and_down_python.py
@@ -64,6 +64,30 @@ lstDirectories = [f for f in listdir(strGamesFolder) if isdir(join(strGamesFolde
 dicResources = {}
 dicResources["imgTitle"] = pygame.image.load("graphics/title.png")
 
+dicResources["imgRoll_1"] = pygame.image.load("graphics/dice/roll_1.png")
+dicResources["imgRoll_2"] = pygame.image.load("graphics/dice/roll_2.png")
+dicResources["imgRoll_3"] = pygame.image.load("graphics/dice/roll_3.png")
+dicResources["imgRoll_4"] = pygame.image.load("graphics/dice/roll_4.png")
+dicResources["imgRoll_5"] = pygame.image.load("graphics/dice/roll_5.png")
+dicResources["imgRoll_6"] = pygame.image.load("graphics/dice/roll_6.png")
+dicResources["imgRoll_7"] = pygame.image.load("graphics/dice/roll_7.png")
+dicResources["imgRoll_8"] = pygame.image.load("graphics/dice/roll_8.png")
+dicResources["imgRoll_9"] = pygame.image.load("graphics/dice/roll_9.png")
+dicResources["imgRoll_10"] = pygame.image.load("graphics/dice/roll_10.png")
+dicResources["imgRoll_11"] = pygame.image.load("graphics/dice/roll_11.png")
+dicResources["imgRoll_12"] = pygame.image.load("graphics/dice/roll_12.png")
+dicResources["imgRoll_13"] = pygame.image.load("graphics/dice/roll_13.png")
+dicResources["imgRoll_14"] = pygame.image.load("graphics/dice/roll_14.png")
+dicResources["imgRoll_15"] = pygame.image.load("graphics/dice/roll_15.png")
+dicResources["imgRoll_16"] = pygame.image.load("graphics/dice/roll_16.png")
+dicResources["imgRoll_17"] = pygame.image.load("graphics/dice/roll_17.png")
+dicResources["imgRoll_18"] = pygame.image.load("graphics/dice/roll_18.png")
+dicResources["imgRoll_19"] = pygame.image.load("graphics/dice/roll_19.png")
+dicResources["imgRoll_20"] = pygame.image.load("graphics/dice/roll_20.png")
+dicResources["imgRoll_21"] = pygame.image.load("graphics/dice/roll_21.png")
+dicResources["imgRoll_22"] = pygame.image.load("graphics/dice/roll_22.png")
+dicResources["imgRoll_23"] = pygame.image.load("graphics/dice/roll_23.png")
+
 dicFont = {}
 dicFont["0"] = pygame.image.load("graphics/fonts/0.png")
 dicFont["1"] = pygame.image.load("graphics/fonts/1.png")
@@ -128,7 +152,10 @@ intNumberOfPlayers = 0 # How many people are playing
 intCurrentPlayer = 0 # The current players turn
 intCurrentTurnState = 0 # Keep track of the steps that must be completed in a turn
 
+tmrDiceRollAnimation = clsSimpleTimer()
+tmrDiceFinishRollAnimation = clsSimpleTimer()
 
+tmrPlayerMoveAnimation = clsSimpleTimer()
 
 # =============================================================================
 # Define game functions
@@ -275,6 +302,27 @@ def movePiece(intNumber, intPlayer):
 	# print(intSelectedElement)
 	# print(dicGameConfig[intSelectedElement].goto)
 
+def findPosition(intNumber):
+	global dicGameConfig
+	
+	# print("Looking for number:")
+	# print(intNumber)
+	
+	blnFound = False
+	
+	for intSelectedElement in range(len(dicGameConfig)):
+		if dicGameConfig[intSelectedElement].number == str(intNumber):
+			blnFound = True
+			break
+	
+	# print("Found on index:")
+	# print(intSelectedElement)
+	
+	if blnFound == True:
+		return intSelectedElement
+	else:
+		return -1
+
 def roleDice():
 	global intFirstDie, intSecondDie
 	
@@ -303,15 +351,28 @@ roleDice()
 
 # Testing Game Logic
 dicPlayers[0] = clsGamePlayer()
-dicPlayers[0].position = 0
+dicPlayers[0].position = 71
 dicPlayers[1] = clsGamePlayer()
-dicPlayers[1].position = 3
+dicPlayers[1].position = 31
+dicPlayers[2] = clsGamePlayer()
+dicPlayers[2].position = 24
 
 intGameState = 5
-intNumberOfPlayers = 2
+intNumberOfPlayers = 3
 intCurrentPlayer = 0
 intCurrentTurnState = 0
 
+intRollDiceFrame = 1
+intRollDiceState = 0
+
+
+intPlayerAnimationPositionX = 0
+intPlayerAnimationPositionY = 0
+intPlayerAnimationPositionToX = 0
+intPlayerAnimationPositionToY = 0
+intPlayerAnimationPosition = 0
+intPlayerAnimationPositionTo = 0
+intPlayerDestinationPosition = 0
 
 while blnRunning:
 
@@ -341,7 +402,10 @@ while blnRunning:
 		drawImage(dicResources["imgPlayer1"], 160, 160, 10, 10)
 		recMousePos = pygame.mouse.get_pos()
 		drawSentence("X " + str(int(float((recMousePos[0])/decScaleGame)-(decOffsetWidth/decScaleGame))) + " Y " + str(int((float(recMousePos[1])/decScaleGame)-(decOffsetHeight/decScaleGame))), 400, 20, 10, 10)
-	
+
+	# =========================================================================
+	# View The Board State (playing the game)
+	# =========================================================================
 	elif (intGameState == 5):
 		drawImage(dicResources["imgTitle"], 0, 0, 250, 40)	
 		drawImage(dicResources["imgBoard"], 0, 0, dicResources["imgBoard"].get_width(), dicResources["imgBoard"].get_height())		
@@ -350,22 +414,135 @@ while blnRunning:
 		recMousePos = pygame.mouse.get_pos()
 		drawSentence("X " + str(int(float((recMousePos[0])/decScaleGame)-(decOffsetWidth/decScaleGame))) + " Y " + str(int((float(recMousePos[1])/decScaleGame)-(decOffsetHeight/decScaleGame))), 400, 20, 10, 10)
 	
+	# =========================================================================
+	# Animate Player Move State (playing the game)
+	# =========================================================================
+	elif (intGameState == 7):
+		if (tmrPlayerMoveAnimation.checkTimePassed(10)):
+			
+			tmrPlayerMoveAnimation.resetTimer()
+			if (intPlayerAnimationPositionX < intPlayerAnimationPositionToX):
+				intPlayerAnimationPositionX = intPlayerAnimationPositionX + 1
+			if (intPlayerAnimationPositionX > intPlayerAnimationPositionToX):
+				intPlayerAnimationPositionX = intPlayerAnimationPositionX - 1	
+			if (intPlayerAnimationPositionY < intPlayerAnimationPositionToY):
+				intPlayerAnimationPositionY = intPlayerAnimationPositionY + 1
+			if (intPlayerAnimationPositionY > intPlayerAnimationPositionToY):
+				intPlayerAnimationPositionY = intPlayerAnimationPositionY - 1	
+			if (intPlayerAnimationPositionY == intPlayerAnimationPositionToY) and (intPlayerAnimationPositionX == intPlayerAnimationPositionToX):	
+				
+				print(">>>>>> We made it to " + str(intNextPosition))
+				
+				
+				
+				# Set the current animation postion to the current player position
+				if intPlayerDestinationPosition == intPlayerAnimationPositionTo:
+					# Check if you won
+					
+					# Otherwise switch to the next player's turn
+					dicPlayers[intCurrentPlayer].position = intNextPosition
+					intCurrentTurnState = 0
+					intCurrentPlayer = intCurrentPlayer + 1
+					if intCurrentPlayer > intNumberOfPlayers - 1:
+						intCurrentPlayer = 0
+					intGameState = 5
+				else:
+					
+					dicPlayers[intCurrentPlayer].position = intNextPosition
+					intPlayerAnimationPositionX = int(dicGameConfig[dicPlayers[intCurrentPlayer].position].x)
+					intPlayerAnimationPositionY = int(dicGameConfig[dicPlayers[intCurrentPlayer].position].y)
+					intPlayerAnimationPosition = int(dicGameConfig[dicPlayers[intCurrentPlayer].position].number)
+					
+					if (intPlayerAnimationPosition > len(dicGameConfig) - 1):
+						print(">>>> Reached the end...")
+						intPlayerDestinationPosition = intPlayerAnimationPosition - (intPlayerDestinationPosition - intPlayerAnimationPosition) 
+						print(">>>> New Destination " + str(intPlayerDestinationPosition))
+						
+					# Find the next step
+					
+					if (intPlayerDestinationPosition > intPlayerAnimationPosition):
+						intNextPosition = findPosition(int(dicGameConfig[dicPlayers[intCurrentPlayer].position].number)+1)
+					else:
+						intNextPosition = findPosition(int(dicGameConfig[dicPlayers[intCurrentPlayer].position].number)-1)
+						
+					intPlayerAnimationPositionToX = int(dicGameConfig[intNextPosition].x)
+					intPlayerAnimationPositionToY = int(dicGameConfig[intNextPosition].y)
+					intPlayerAnimationPositionTo = int(dicGameConfig[intNextPosition].number)
+					
+					
+						
+				
+		drawImage(dicResources["imgTitle"], 0, 0, 250, 40)	
+		drawImage(dicResources["imgBoard"], 0, 0, dicResources["imgBoard"].get_width(), dicResources["imgBoard"].get_height())		
+		for intPlayers in range(intNumberOfPlayers):
+			if (intPlayers == intCurrentPlayer):
+				drawImage(dicResources["imgPlayer"+str(intPlayers+1)], intPlayerAnimationPositionX-5, intPlayerAnimationPositionY-5, 10, 10)	
+			else:
+				drawImage(dicResources["imgPlayer"+str(intPlayers+1)], float(dicGameConfig[dicPlayers[intPlayers].position].x)-5, float(dicGameConfig[dicPlayers[intPlayers].position].y)-5, 10, 10)	
+		recMousePos = pygame.mouse.get_pos()
+		drawSentence("X " + str(int(float((recMousePos[0])/decScaleGame)-(decOffsetWidth/decScaleGame))) + " Y " + str(int((float(recMousePos[1])/decScaleGame)-(decOffsetHeight/decScaleGame))), 400, 20, 10, 10)
 	
+	
+	# =========================================================================
+	# Dice Roll State 
+	# =========================================================================
+	elif (intGameState == 6):
+		drawImage(dicResources["imgTitle"], 0, 0, 250, 40)
+		if (tmrDiceRollAnimation.checkTimePassed(80)):
+			intRndDie1 = random.randint(1, 6)
+			intRndDie2 = random.randint(1, 6)
+			intRollDiceFrame = intRollDiceFrame + 1
+			if intRollDiceFrame > 23:
+				intRollDiceFrame = 1
+			tmrDiceRollAnimation.resetTimer()
+		
+		drawImage(dicResources["imgRoll_" + str(intRollDiceFrame)], intGameWidth - 50, intGameHeight - 50, 50, 50)
+		
+		if intRollDiceState == 0:
+			drawImage(dicFont[str(intRndDie1)], 170, 200, 50, 50)
+			drawImage(dicFont[str(intRndDie2)], 250, 200, 50, 50)
+			drawSentence("PRESS SPACE TO ROLL THE DICE", 100, 300, 10, 10)
+		elif intRollDiceState == 1:
+			tmrDiceFinishRollAnimation.resetTimer()
+			intRollDiceState = 2
+		elif intRollDiceState == 2:
+			drawImage(dicFont[str(intFirstDie)], 170, 200, 50, 50)
+			drawImage(dicFont[str(intSecondDie)], 250, 200, 50, 50)
+			drawSentence("YOUR TOTAL IS " + str(intFirstDie + intSecondDie), 100, 300, 10, 10)
+			if (tmrDiceFinishRollAnimation.checkTimePassed(2000)):
+				print("Finished animation")
+				intRollDiceFrame = 1
+				intRollDiceState = 0
+				
+				# Set the current animation postion to the current player position
+				intPlayerAnimationPositionX = int(dicGameConfig[dicPlayers[intCurrentPlayer].position].x)
+				intPlayerAnimationPositionY = int(dicGameConfig[dicPlayers[intCurrentPlayer].position].y)
+				intPlayerAnimationPosition = int(dicGameConfig[dicPlayers[intCurrentPlayer].position].number)
+				intPlayerDestinationPosition = intPlayerAnimationPosition + intFirstDie + intSecondDie
+				# Find the next step
+				intNextPosition = findPosition(intPlayerAnimationPosition+1)
+				intPlayerAnimationPositionToX = int(dicGameConfig[intNextPosition].x)
+				intPlayerAnimationPositionToY = int(dicGameConfig[intNextPosition].y)
+				intPlayerAnimationPositionTo = int(dicGameConfig[intNextPosition].number)
+				
+				print(">>>> We are going from " + str(intPlayerAnimationPosition) + " to " + str(intPlayerDestinationPosition))
+				
+				intGameState = 7 # Animate the player movement
+			
+		
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			blnRunning = False
 		elif event.type == pygame.KEYDOWN:
 			if (event.key == pygame.K_SPACE):
+				if (intGameState == 6):
+					if (intRollDiceState == 0):
+						intRollDiceState = 1
 				if (intGameState == 5):
 					if intCurrentTurnState == 0:
 						roleDice();
 						intCurrentTurnState = 1
-					elif intCurrentTurnState == 1:
-						movePiece(int(dicGameConfig[dicPlayers[intCurrentPlayer].position].number) + intFirstDie + intSecondDie, intCurrentPlayer)
-						intCurrentPlayer = intCurrentPlayer + 1
-						if intCurrentPlayer > intNumberOfPlayers - 1:
-							intCurrentPlayer = 0
-						intCurrentTurnState = 0
+						intGameState = 6 # roll dice animation
 		elif event.type == VIDEORESIZE:
 			resize_display()
 		
